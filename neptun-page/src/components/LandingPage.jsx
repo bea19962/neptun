9f1af92e465cbf2c4b5f6c7520bec565e4a04088
BananaGrill <bea_19962@hotmail.com>
2024-11-09 12:38:34 +0000

    test

--- neptun-page/src/components/LandingPage.jsx
+++ neptun-page/src/components/LandingPage.jsx
@@ -1,53 +1,40 @@
-import { useEffect, useRef } from "react";
+import { useEffect } from "react";
 import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 import "../styles/_landingpage.scss";
-import BaseButton from "./BaseButton";
 
 gsap.registerPlugin(ScrollTrigger);
 
 const LandingPage = () => {
-  const maskRef = useRef();
-
-  const scrollToNextSection = () => {
-    document.getElementById("projects").scrollIntoView({ behavior: "smooth" });
-  };
-
   useEffect(() => {
-    gsap.to(maskRef.current, {
-      scale: 30,  // Scale factor for enlarging the image
-      duration: 1,
-      transformOrigin: "center center", // Origin of scaling
-      scrollTrigger: {
-        trigger: maskRef.current,
-        start: "top top", // When the mask hits the top of the viewport
-        end: "+=100%", // How far the animation should last
-        scrub: true, // Scroll-linked animation
-        pin: true, // Pin the image in place as it animates
-        pinSpacing: false, // Prevent additional spacing
-        markers:true,
-      }
-    });
+    gsap
+      .timeline({
+        scrollTrigger: {
+          trigger: ".landing-page",
+          markers: {
+            startColor: "yellow",
+            endColor: "#42a6e0",
+            fontSize: "14px",
+          },
+          start: "top top",
+          end: "+=1", // Limits the scroll-triggered animation to 100px
+          pin: true,
+          scrub: 1,
+        },
+      })
+      .to(".content", {
+        "clip-path": "polygon(-100% 0%, 100% 0%, 100% 100%, -25% 100%)",
+        ease: "none",
+        stagger: 0.5,
+      });
   }, []);
-
   return (
-    <section className="landing-page">
-      <div id="mask" ref={maskRef}>
-        <img
-          src="../src/assets/vecteezy_black-square-png_1209957.png"
-          alt="Mask"
-        />
-      </div>
+    <div className="landing-page">
       <div className="content">
-        <div className="text-left">
-          <h1>Welcome to Neptun Interactive</h1>
-        </div>
-        <div className="text-right">
-          <p>We are a studio based in Cologne</p>
-        </div>
-        <BaseButton onClick={scrollToNextSection}>Scroll Down</BaseButton>
+        <h1>Content Revealed Below</h1>
+        <p>Welcome to the hidden content.</p>
       </div>
-    </section>
+    </div>
   );
 };
 
